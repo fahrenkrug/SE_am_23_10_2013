cfab273c74cd02c9935eb09e65fc304db1d26602
Finn Fahrenkrug <finnfahrenkrug@gmail.com>
2013-10-23 14:35:43 +0200

    implemented subtraktion

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,13 @@
 public class Calculator{
-	public void main (String[] args){
+	public static void main (String[] args){
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte eingeben [+-] a b:");
+		String operation = scanner.next();
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		if (operation.equals("-")){
+			int result = a - b;
+			System.out.println(a + operation + b + "=" + result);
+		}
 	}
 }
