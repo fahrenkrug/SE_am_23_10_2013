bb678e534030a03287b3268b668d7eed550e8061
Finn Fahrenkrug <finnfahrenkrug@gmail.com>
2013-10-23 14:28:55 +0200

    implemented addition

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator{
-	public void main (String[] args){
+	public static void main (String[] args){
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte eingeben [+-] a b");
+		String operation = scanner.next();
+		int a = scanner.nextInt();
+		int b = scaner.nextInt();
+		if (operation.equals("+")){
+			int result = a+b;
+			System.out.println(a + "+" + b + "=" + result);
 	}
 }
